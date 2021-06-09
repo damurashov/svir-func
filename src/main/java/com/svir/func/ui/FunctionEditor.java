c74adc1682288567b44bffe6f1fc8a4e3670e216
DmitryMurashov <dmtr.murashov@gmail.com>
2021-06-09 23:07:50 +0300

    [ui.FunctionEditor] Placing | The configuration seems finished

---
+++ src/main/java/com/svir/func/ui/FunctionEditor.java
@@ -0,0 +1,18 @@
+package com.svir.func.ui;
+
+import javax.swing.*;
+
+public class FunctionEditor {
+    private JPanel panelRoot;
+    private JSplitPane functionSplitPane;
+    private JPanel plotPanel;
+    private JSplitPane splitPaneRoot;
+    private JPanel detailsFunctionPanel;
+    private JList fragmentsList;
+    private JScrollPane fragmentsPanel;
+    private JComboBox typeFunctionComboBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
--- src/main/java/com/svir/func/ui/FunctionPlot.java
+++
@@ -1,6 +0,0 @@
-package com.svir.func.ui;
-
-import javax.swing.*;
-
-public class FunctionPlot {
-}
