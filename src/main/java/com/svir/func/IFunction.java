9f4a75891800688a2cf3a2416bde4f0b2399f237
DmitryMurashov <dmtr.murashov@gmail.com>
2021-06-22 00:22:58 +0300

    [com.svir.func] IFunction, function interface | Simplified it, greatly

--- src/main/java/com/svir/func/IFunction.java
+++ src/main/java/com/svir/func/IFunction.java
@@ -1,40 +1,12 @@
 package com.svir.func;
 
-import javafx.util.Pair;
+import com.svir.func.Bound;
+import com.svir.func.Interval;
 
 public interface IFunction {
 
-    /**
-     * Represents a bound of a mathematical interval abstraction: (VALUE, OPEN/CLOSED)
-     */
-    public static class Bound {
-        private double val;
-        private boolean open;
+	public Interval getInterval();
 
-        public double getVal() {
-            return val;
-        }
-
-        public boolean isOpen() {
-            return open;
-        }
-
-        Bound(float aVal, boolean aOpen) {
-            val = aVal;
-            open = aOpen;
-        }
-    }
-
-    /**
-     * The Interval of the function
-     * @return  Pair<LEFT BOUND, RIGHT BOUND>
-     */
-    public Pair<Bound, Bound> getInterval();
-
-    /**
-     * Calculates value in the point "x"
-     * @param x  -  the point
-     * @return  -  {double, Nan, +-Inf}; returns NaN if unable to compute, or "x" exceeds the interval
-     */
-    public double evaluate(double x);
+	public double evaluate(double x);
 }
+
